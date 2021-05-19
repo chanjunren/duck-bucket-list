c822a8b1187fb3f94e390afa0ae341eee236945b
chanjunren <chanjunren@gmail.com>
2021-05-19 16:41:51 +0800

    Added logic for handling form validity

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,37 +1,91 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
 
 import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/FormValidators';
 
 import './NewPlace.css'
 
-const NewPlace = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {
-    
-  }, []);
+const initialState = {
+  inputs: {
+    titleInput: {
+      value: '',
+      isValid: false
+    }, 
+    descriptionInput: {
+      value: '',
+      isValid: false
+    }
+  },
+  isValid: false
+};
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {
-    
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+
+      const newState = {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid
+          }
+        },
+        isValid: formIsValid
+      };
+
+      console.log(newState);
+
+      return newState; 
+    default:
+      return state;
+  }
+};
+
+const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, initialState)
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      inputId: id,
+      value: value,
+      isValid: isValid
+    });
   }, []);
 
   return <form className="place-form">
     <Input
-      id="title-input" 
+      id="titleInput" 
       element="input" 
       type="text"
-      onInput={titleInputHandler}
+      onInput={inputHandler}
       label="Title"
       validators={[VALIDATOR_REQUIRE()]}
       errorText="Please enter a valid title"/>
     <Input 
-      id="description-input"
+      id="descriptionInput"
       element="textarea" 
       type="text" 
-      onInput={descriptionInputHandler}
+      onInput={inputHandler}
       label="Description"
       validators={[VALIDATOR_MINLENGTH(10)]}
       errorText="Please enter a description of at least 10 characters :)"/>
+      <Button
+        type="submit"
+        disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
   </form>
 };
 
