d576b7364565700b20716802e60baf135f93d4e8
Jun Ren <chanjunren@gmail.com>
2021-06-02 13:57:39 +0800

    Integrated UserPlaces

--- client/src/places/pages/UserPlaces.js
+++ client/src/places/pages/UserPlaces.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
+import ErrorModal from '../../shared/components/uiElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/uiElements/LoadingSpinner';
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Himalayans',
-    description: 'Legendary mountain range featuring numerous towering peaks, including Mount Everest!',
-    imageUrl:
-      'https://deih43ym53wif.cloudfront.net/everest-base-camp-himalayas-nepal_08bc81b2f2.jpeg',
-    address: 'Mountain Range (India, Pakistan, Afghanistan, China, Bhutan and Nepal)',
-    location: {
-      lat: 30.0925684,
-      lng: 76.5389941
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'The Lofoten Islands',
-    description: 'Lofoten is known for excellent fishing, nature attractions such as the northern lights and the midnight sun, and small villages off the beaten track',
-    imageUrl:
-      'https://img.traveltriangle.com/blog/wp-content/uploads/2018/11/lofoten1.jpg',
-    address: '8314 Gimsøysand, Norway',
-    location: {
-      lat: 68.4716466,
-      lng: 13.8568658
-    },
-    creator: 'u1'
-  }
-];
+import useHttpClient from '../../shared/components/hooks/HttpHook';
 
 const UserPlaces = () => {
+  const { isLoading, errorEncountered, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-  return <PlaceList items={loadedPlaces} />;
+
+  useEffect(() => {
+    const fetchUserPlaces = async () => {
+      try {
+        console.log(`Sending to: http://localhost:5000/api/places/user/${userId}`);
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        setLoadedPlaces(responseData.user_places);
+
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchUserPlaces();
+  }, [sendRequest])
+
+
+
+  return <React.Fragment>
+    <ErrorModal errorMessage={errorEncountered} onClear={clearError}/>
+    {isLoading && (<div className="center">
+      <LoadingSpinner />
+    </div>)}
+    {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+  </React.Fragment>;
 };
 
 export default UserPlaces;
