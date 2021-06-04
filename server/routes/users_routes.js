87297ccf2fac5cdef5df79ff26c024c91ac6fa86
Jun Ren <chanjunren@gmail.com>
2021-06-04 14:17:04 +0800

    Added middleware for storing images

--- server/routes/users_routes.js
+++ server/routes/users_routes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const userController = require('../controllers/users_controller');
+const fileUpload = require('../middleware/file_upload');
+
 
 const router = express.Router();
 
 router.get('/', userController.getAllUsers);
 
 router.post('/signup',
+    fileUpload.single('image'),
     [check('email').normalizeEmail().isEmail(),
     check('name').notEmpty(),
     check('password').isLength({ min: 6 })],
